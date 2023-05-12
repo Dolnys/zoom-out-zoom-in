b2be3ba2b175e93c8788161a88b810786e0120d5
Dolnys <thedolnys@gmail.com>
2023-05-12 12:16:04 +0200

    final commit

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 const sizeUp = document.querySelector('.sizeUp');
 const sizeDown = document.querySelector('.sizeDown');
 const colorBtn = document.querySelector('.color');
+const p = document.querySelector('p');
+let fontSize = 36;
+
+const increase = () => {
+  if (fontSize >= 61) {
+    return;
+  }
+  fontSize += 5;
+  p.style.fontSize = fontSize + 'px';
+};
+
+sizeUp.addEventListener('click', increase);
+
+const decrease = () => {
+  if (fontSize <= 21) {
+    return;
+  }
+  fontSize -= 5;
+  p.style.fontSize = fontSize + 'px';
+};
+
+sizeDown.addEventListener('click', decrease);
+
+const colorChanger = () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  p.style.color = `rgb(${r}, ${g}, ${b})`;
+};
+
+colorBtn.addEventListener('click', colorChanger);
